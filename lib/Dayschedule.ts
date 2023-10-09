f529ccaddd3c69b037e8d56b17a8592ebe9642db
jainshubh0612 <shubhjain56854@gmail.com>
2023-10-09 16:56:19 +0530

    --

--- lib/Dayschedule.ts
+++ lib/Dayschedule.ts
@@ -9,32 +9,16 @@ export type Resource = {
     deleteResource: (resource_id: string) => any;
 }
 
-export interface dayschedule {
-    resource: Resource,
-}
-
 class Dayschedule {
     private apiKey: string;
     public apiUrl: string = "https://api.dayschedule.in/v1";
 
     public resource: Resource;
-    // private _headers: AxiosRequestConfig;
 
     constructor(apiKey: string) {
         this.apiKey = apiKey;
         this.resource = resource(apiKey, this.apiUrl);
     }
-
-//    async addResources(): Promise<dayschedule> {
-//         console.log(resource(this.apiKey, this.apiUrl))
-//     //    return Object.assign(this, {
-//     //         resource: resource(this.apiKey, this.apiUrl)
-//     //     })
-
-//     return {
-//         resource: resource(this.apiKey, this.apiUrl)
-//      }
-//     }
 }
 
 export default Dayschedule;
